0344cf518c44df36d28b356a333e83c0eac4b68e
approachcircle <osc-mcd@protonmail.com>
2024-06-06 21:38:07 +0100

    initialise project ready for writing

--- client/src/main/java/net/approachcircle/client/Main.java
+++ client/src/main/java/net/approachcircle/client/Main.java
@@ -1,7 +1,5 @@
 package net.approachcircle.client;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("testing client");
-    }
+    public static void main(String[] args) {}
 }
--- server/src/main/java/net/approachcircle/server/Main.java
+++ server/src/main/java/net/approachcircle/server/Main.java
@@ -1,7 +1,5 @@
 package net.approachcircle.server;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("testing server");
-    }
+    public static void main(String[] args) {}
 }
